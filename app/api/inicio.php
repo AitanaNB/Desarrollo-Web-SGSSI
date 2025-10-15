efa6c90f8879c196e31d3e5d51a70f7c2fb055e4
AitanaNB <anino006@ikasle.ehu.eus>
2025-10-16 00:36:00 +0200

    Página de inicio funcional.
    -No es lo más bonito, pero funciona.

--- app/api/inicio.php
+++ app/api/inicio.php
@@ -1,10 +1,39 @@
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <title>COMPRAMOS TU COCHE</title>
     <link rel="stylesheet" href="/css/style.css">
+    <link rel="shortcut icon" href="/media/icon.svg" />
 </head>
 <body>
-    <h2>Página principal temporal para pruebas.</h2>
+    
+     <header>
+         <!-- Botón arriba a la izquierda -->
+         <a href="cuenta.html" class="boton_Superior">  
+        <img src="/media/a.png">
+        </a>
+        
+        <h1>FORO COMPRAMOS TU COCHE</h1>
+        <p>Bienvenido a tu portal automotriz</p>       
+    </header>
+    <nav>
+        <div class="contenido">
+                <h2>Tu portal de coches favorito</h2>
+                <p>Explora el catálogo, revisa tus vehículos o accede a tu cuenta.</p>
+
+                <div id="botones">
+                    <a href="catalogo.html" class="boton">Catálogo de coches</a>
+                    <br>
+
+                    <a href="mis-coches.html" class="boton">Ver mis coches</a>
+                </div>
+    </nav>
+    
+<footer>
+    <!--&copy; <?= date('Y') ?> <br>-->
+     <a href="https://github.com/AitanaNB/Desarrollo-Web-SGSSI">Nuestro maravilloso y organizado código está disponible en Github</a>
+     
+</footer>
 </body>
