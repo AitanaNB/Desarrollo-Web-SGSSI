14965e5ce9fa11c3cd4c210def087d9eefa93d2a
AitanaNB <anino006@ikasle.ehu.eus>
2025-10-16 11:51:59 +0200

    Cambios de nombre

---
+++ app/api/register.php
@@ -0,0 +1,35 @@
+<?php
+// Conexión a la base de datos
+include 'bdcon.php'; // Este archivo debe definir: $conn
+
+// Recibir datos del formulario
+$nombre = $_POST['nombre'];
+$apellidos = $_POST['apellidos'];
+$dni = $_POST['dni'];
+$telefono = $_POST['telefono'];
+$fecha_nacimiento = $_POST['fecha_nacimiento'];
+$email = $_POST['email'];
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// Validación simple
+if (empty($nombre) || empty($email) || empty($password)) {
+    die("Todos los campos son obligatorios.");
+}
+
+/// Insertar en la base de datos
+//$stmt = $conn->prepare("INSERT INTO usuarios (nombre, email) VALUES (?, ?)");
+//$stmt->bind_param("ss", $nombre, $email); version xa evitar injeccion?
+
+$sql = "INSERT INTO usuarios
+  (nombre, apellidos, dni, telefono, fecha_nacimiento, email, username, password)
+VALUES (
+  '$nombre', '$apellidos', '$dni', $telefono, '$fecha_nacimiento', '$email', '$username', '$password'
+    )";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Usuario registrado con éxito";
+} else {
+    echo "Error: " . $conn->error;
+}
+?>
